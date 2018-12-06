726fab03274cc67a8a63bd61ccc8aecd89178a00
bondrogeen <bondrogeen@gmail.com>
2018-12-06 10:02:41 +0300

    rebuild

--- files/Defender_X55_comm.lua
+++ files/Defender_X55_comm.lua
@@ -1,10 +1,6 @@
-
 local function bitToNumber(str)
  local b,i=0,1
- for s in str:reverse():gmatch (".") do
-  b=b+s*i
-  i=i*2
- end;
+ for s in str:reverse():gmatch(".")do b=b+s*i i=i*2 end
  return b
 end
 
