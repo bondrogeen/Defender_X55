487f54f51e480828fa1ff5f0b936a97d3bc8d9e8
bondrogeen <bondrogeen@gmail.com>
2018-12-06 14:22:14 +0300

    minor fix

--- files/Defender_X55.lua
+++ files/Defender_X55.lua
@@ -20,36 +20,19 @@ return o
 end
 
 local function sort(m,v)
- local r=false
+ local r
  if not tonumber(v) then return false end
  v=tonumber(v)
  m=string.lower(m)
- if m=="volume"then
-  r=(v<=63 and v>=0)and set(63-v)
- elseif m=="left"then
-  r=(v<=31 and v>=0)and set(128+31-v)
- elseif m=="right"then
-  r=(v<=31 and v>=0)and set(160+31-v)
- elseif m=="lr"then
-  r=(v<=31 and v>=0)and set(192+31-v)
- elseif m=="subwoofer" then
-  r=(v<=31 and v>=0)and set(224+31-v)
- elseif m=="mute" then
-  gpio.write(2,v) r=v
- elseif m=="input" then
-  r = (v<=3 and v>=1)and set(79+v)
- elseif m=="bass" then
-  if v<=7 and v>=0 then
-   r = set(96+v)
- elseif v<=15 and v>=8 then
-   r = set(111+8-v)
-  end
- elseif m=="treble" then
-  if v<=7 and v>=0 then
-   r = set(112+v)
-  elseif v<=15 and v>=8 then
-   r = set(127+8-v)
-  end
+ if m=="volume"then r=(v<=63 and v>=0)and set(63-v)
+ elseif m=="left"then r=(v<=31 and v>=0)and set(128+31-v)
+ elseif m=="right"then r=(v<=31 and v>=0)and set(160+31-v)
+ elseif m=="lr"then r=(v<=31 and v>=0)and set(192+31-v)
+ elseif m=="subwoofer"then r=(v<=31 and v>=0)and set(224+31-v)
+ elseif m=="mute"then if (v==1 or v==0) then gpio.write(2,v) r=v else r=false end
+ elseif m=="input"then r=(v<=3 and v>=1)and set(79+v)
+ elseif m=="bass"then r=(v<=7 and v>=0) and set(96+v) or (v<=15 and v>=8) and set(111+8-v)
+ elseif m=="treble"then r=(v<=7 and v>=0) and set(112+v) or (v<=15 and v>=8) and set(127+8-v)
  end
  if r then save(m,v)if _M then _M:pub("info/Defender_X55/"..m,v)end end
  return r
@@ -59,7 +42,7 @@ return function (t)
  local x,d,r = {}
   for k,v in pairs(t) do
    d=sort(k,v)
-   if d then x[k]=d r=1 end
+   if d~=nil then x[k]=d r=1 end
   end
- return r and x
+ return  x
 end
